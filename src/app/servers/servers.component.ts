ab372e2cbf948ffdf709b956db8c90edd45b0d08
Khushboo Thakur <khushboothakur2305@gmail.com>
2021-04-29 01:15:00 +0530

    Update

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent implements OnInit {
+export class ServersComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
   @Input() name: string;
   allowNewServer = false;
   Server = 'Cannot Add Server';
@@ -19,9 +37,31 @@ export class ServersComponent implements OnInit {
     }, 2000);
   }
   onAddServer() {
+    console.log('constructor');
+
     this.Server = 'Server Added';
     this.serverCreationStatus = `${this.ServerName} Server Was Created`;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('onInit');
+  }
+  ngDoCheck() {
+    console.log('DoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('after content init');
+  }
+  ngAfterContentChecked() {
+    console.log('after content checked');
+  }
+  ngAfterViewInit() {
+    console.log('after view checked');
+  }
+  ngAfterViewChecked() {
+    console.log('after view checked');
+  }
+  ngOnDestroy() {
+    console.log('on destroy');
+  }
 }
