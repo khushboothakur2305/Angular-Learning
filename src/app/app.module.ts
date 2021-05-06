a84b7958a270364678fd2a8ebbb85c98f86150ed
Khushboo Thakur <khushboothakur2305@gmail.com>
2021-05-06 18:43:15 +0530

    featI(routing): Routing Section update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 import { ServerComponent } from './Server/server.component';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { GameControlComponent } from './Assignment4/game-control/game-control.component';
 import { OddComponent } from './Assignment4/odd/odd.component';
 import { EvenComponent } from './Assignment4/even/even.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { UnlessDirective } from './unless/unless.directive';
+import { HomeComponent } from './Routing/home/home.component';
+import { RoutingServersComponent } from './Routing/servers/servers.component';
+import { UsersComponent } from './Routing/users/users.component';
+import { UserComponent } from './Routing/users/user/user.component';
+import { EditServerComponent } from './Routing/servers/edit-server/edit-server.component';
+import { ServersService } from './Routing/servers/servers.service';
+import { RoutingServerComponent } from './Routing/servers/server/server.component';
+const router: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: RoutingServersComponent },
+  { path: '', component: HomeComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +36,20 @@ import { UnlessDirective } from './unless/unless.directive';
     BasicHighlightDirective,
     BetterHighlightDirective,
     UnlessDirective,
+    HomeComponent,
+    RoutingServersComponent,
+    UsersComponent,
+    UserComponent,
+    EditServerComponent,
+    RoutingServerComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(router),
+  ],
+  providers: [ServersService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
